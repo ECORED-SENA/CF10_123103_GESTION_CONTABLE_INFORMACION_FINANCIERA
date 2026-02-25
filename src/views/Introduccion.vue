26f1ae13854fdd4995407eebaf63ca18058e70e0
Henry Álvarez Astudillo <halvarezast@gmail.com>
2026-02-25 07:47:41 -0500

    Update747

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,13 +19,13 @@
           .col-lg-auto.d-none.d-lg-block
             img(src="@/assets/curso/intro/2.svg", style="max-width: 90px").mx-auto
           .col-lg
-            p.mb-0 En el presente componente podrá conocer los conceptos referentes a los inventarios destinados para la venta, así mismo, le permite aplicar los sistemas de inventarios, sus correspondientes métodos de valuación; al igual que los instrumentos financieros utilizados en operaciones de compra y venta de inventarios, con la respectiva carga impositiva, de acuerdo con la normatividad y políticas contables vigentes.
+            p.mb-0 En el presente componente podrá conocer los conceptos referentes a los inventarios destinados para la venta; así mismo, le permite aplicar los sistemas de inventarios, sus correspondientes métodos de evaluación; al igual que los instrumentos financieros utilizados en operaciones de compra y venta de inventarios, con la respectiva carga impositiva, de acuerdo con la normatividad y políticas contables vigentes.
     
         .bg-color-4.mb-5(data-aos="fade-up")
           .row.justify-content-center.align-items-center
             .col-lg
               .p-4
-                p.mb-0(data-aos="fade-up") #[strong Nota aclaratoria:] las tablas que se toman como referencia en los ejemplos de cada uno de los temas desarrollados, elaboradas mediante el programa Excel son derivadas de estos ejercicios, la elaboración es propia del autor del presente componente, por lo tanto, no se numeran y no se establece un nombre específico.
+                p.mb-0(data-aos="fade-up") #[strong Nota aclaratoria:] las tablas que se toman como referencia en los ejemplos de cada uno de los temas desarrollados, elaboradas mediante el programa Excel son derivadas de estos ejercicios; la elaboración es propia del autor del presente componente, por lo tanto, no se numeran y no se establece un nombre específico.
             .col-lg-auto.d-none.d-xl-block
               figure
                 img(src='@/assets/curso/intro/3.svg', alt='')
